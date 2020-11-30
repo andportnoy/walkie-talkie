528683cb3dcf2d1b4e391f1833012c0ed5e81d32
Andrey Portnoy <aportnoy@ucsd.edu>
2020-11-29 23:47:12 -0800

    Print available audio devices

--- audio.c
+++ audio.c
@@ -1,13 +1,69 @@
-void devprint(const PaDeviceInfo *dev) {
-	printf("%s:", dev->name);
-	printf(" srate=%f", dev->defaultSampleRate);
-	printf(" input=(%d)", dev->maxInputChannels);
-	printf("[%.2fms, %.2fms]",
-	  1000*dev->defaultLowInputLatency, 1000*dev->defaultHighInputLatency);
-	printf(" output=(%d)", dev->maxOutputChannels);
-	printf("[%.2fms, %.2fms]",
-	  1000*dev->defaultLowOutputLatency,1000*dev->defaultHighOutputLatency);
-	printf("\n");
+void tableprint(char ***table, int m, int n) {
+	int colwidth[n];
+	memset(colwidth, 0, sizeof colwidth);
+	char *(*t)[n] = (char *(*)[n]) table;
+
+	for (int i=0; i<m; ++i)
+	for (int j=0; j<n; ++j) {
+		int k = strlen(t[i][j]);
+		colwidth[j] = colwidth[j]<k? k: colwidth[j];
+	}
+	for (int i=0; i<m; ++i) {
+		for (int j=0; j<n; ++j) {
+			printf(j? " %*s": "%*s", colwidth[j], t[i][j]);
+		}
+		printf("\n");
+	}
+
+}
+
+void devprint(void) {
+	char *table[100][9] = {
+		{"name", "srate,Hz", "ichans", "iminlat,ms", "imaxlat,ms",
+		 "ochans", "ominlat,ms", "omaxlat,ms", "default"},
+	};
+
+	int m = 1+Pa_GetDeviceCount();
+	int n = sizeof table[0]/sizeof table[0][0];
+	int id = Pa_GetDefaultInputDevice();
+	int od = Pa_GetDefaultOutputDevice();
+	for (int i=1; i<m; ++i) {
+		char buf[1024];
+		int d = i-1;
+		const PaDeviceInfo *dev = Pa_GetDeviceInfo(d);
+		int j = 0;
+
+		sprintf(buf, "%s", dev->name);
+		table[i][j++] = strdup(buf);
+
+		sprintf(buf, "%.0f", dev->defaultSampleRate);
+		table[i][j++] = strdup(buf);
+
+		sprintf(buf, "%d", dev->maxInputChannels);
+		table[i][j++] = strdup(buf);
+
+		sprintf(buf, "%.2f", 1000*dev->defaultLowInputLatency);
+		table[i][j++] = strdup(buf);
+
+		sprintf(buf, "%.2f", 1000*dev->defaultHighInputLatency);
+		table[i][j++] = strdup(buf);
+
+		sprintf(buf, "%d", dev->maxOutputChannels);
+		table[i][j++] = strdup(buf);
+
+		sprintf(buf, "%.2f", 1000*dev->defaultLowOutputLatency);
+		table[i][j++] = strdup(buf);
+
+		sprintf(buf, "%.2f", 1000*dev->defaultHighOutputLatency);
+		table[i][j++] = strdup(buf);
+
+		sprintf(buf, d==id? (d==od? "input,output": "input"):
+				    (d==od? "output"      : ""     ));
+		table[i][j++] = strdup(buf);
+
+		assert(j == n);
+	}
+	tableprint((char ***)table, m, n);
 }
 
 int main() {
@@ -17,14 +73,7 @@ int main() {
 	/* initialize */
 	pacheck(Pa_Initialize(), "initialize");
 
-	for (int i=0, n=Pa_GetDeviceCount(); i<n; ++i)
-		devprint(Pa_GetDeviceInfo(i));
-
-	puts("default input:");
-	devprint(Pa_GetDeviceInfo(Pa_GetDefaultInputDevice()));
-
-	puts("default output:");
-	devprint(Pa_GetDeviceInfo(Pa_GetDefaultOutputDevice()));
+	devprint();
 
 	/* open stream */
 	/* start stream */
